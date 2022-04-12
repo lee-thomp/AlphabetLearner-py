1250c0be47f74c56b2d4c3a23976a8b2421c6c44
lee-thomp <lee.p.thomp@gmail.com>
2022-04-12 22:13:14 +0100

    sorted ordinal indicators on numbers, altered some strings

--- alphabet_learner.py
+++ alphabet_learner.py
@@ -19,11 +19,6 @@ def signal_handler(signal, frame):
 # Attach exit handler
 signal.signal(signal.SIGINT, signal_handler)
 
-# Escape codes for bold and non-bold
-BOLD    = '\033[1m'
-RESET   = '\033[m'
-GREEN   = '\033[32m'
-
 
 # Main function
 def main ():
@@ -31,6 +26,14 @@ def main ():
     # Pull in score counters as global
     global correct_count
     global false_count
+    
+    # Escape codes for bold and non-bold
+    BOLD    = '\033[1m'
+    RESET   = '\033[m'
+    GREEN   = '\033[32m'
+
+    # Ordinal indicator suffixes
+    indicator = ('', 'st', 'nd', 'rd') + ('th',)*17 + ('st', 'nd', 'rd') + ('th',)*3
 
     print('''
     === Interactive Alphabet Game ===
@@ -45,16 +48,16 @@ def main ():
 
         # Flip a coin on whether to ask for letter or position
         if (random.choice([True, False])):
-            answer = input(f"{BOLD}Position of the letter '{letter}':\t{RESET}")
+            answer = input(f"{BOLD}Position of '{letter}':\t{RESET}")
         else:
-            answer = input(f"{BOLD}The letter at position {position}:\t{RESET}")
+            answer = input(f"{BOLD}The {position}{indicator[position]} letter:\t{RESET}")
 
         # Repeat back if answer was correct or not
         if answer in [str(position), letter]:
-            print(f"{BOLD}{GREEN}Correct{RESET}")
+            print(f"{BOLD}{GREEN}Correct{RESET}, '{letter}' is {position}{indicator[position]} in the alphabet")
             correct_count += 1
         else:
-            print(f"False, '{letter}' is {position} in the alphabet")
+            print(f"False, '{letter}' is {position}{indicator[position]} in the alphabet")
             false_count += 1
 
         # Empty line
